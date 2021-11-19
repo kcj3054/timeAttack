d4cc54038c32e25924c64441f3c33ef87c5e1abf
kcj3054 <kcj3054@naver.com>
2021-11-20 01:53:15 +0900

    timeAttack

--- src/main/java/com/example/springtimeattack1119/domain/FormController.java
+++ src/main/java/com/example/springtimeattack1119/domain/FormController.java
@@ -2,16 +2,19 @@ package com.example.springtimeattack1119.domain;
 
 import com.example.springtimeattack1119.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class FormController {
@@ -19,40 +22,58 @@ public class FormController {
     private final MemberRepository memberRepository;
     private final ModelMapper modelMapper;
 
+    @GetMapping("/")
+    public String getForm() {
+        log.info("log");
+        return "index";
+    }
+
     @PostMapping
     @ResponseBody
-    ResponseEntity<MemberDTO> post(@RequestBody MemberDTO memberDTO) {
-        Member memebr = modelMapper.map(memberDTO, Member.class);
-
-        memberRepository.save(memebr);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberDTO);
-    }
-
-    @GetMapping
-    @ResponseBody
-    ResponseEntity<List<MemberDTO>> get() {
-        List<Member> member = memberRepository.findAll();
-        List<MemberDTO> memberDTOList = member.stream()
-                .map(m -> modelMapper.map(m, MemberDTO.class))
-                .collect(Collectors.toList());
-
-        return ResponseEntity.status(HttpStatus.OK).body(memberDTOList);
-    }
-
-    @PutMapping("/{id}")
-    @ResponseBody
-    ResponseEntity<MemberDTO> put(@PathVariable Long id, @RequestBody MemberDTO memberDTO) {
-
-        Optional<Member> memberOp = memberRepository.findById(id);
-        Member member = memberOp.orElseThrow();
-
-        modelMapper.map(memberDTO, member);
+    public String post(@RequestBody Member member, Model model) {
 
         memberRepository.save(member);
 
-        return ResponseEntity.status(HttpStatus.OK).body(memberDTO);
+        List<Member> allMember = memberRepository.findAll();
+        model.addAttribute("allMember", allMember);
+        return "ok";
+
     }
+
+
+
+//    @GetMapping
+//    @ResponseBody
+//    ResponseEntity<List<MemberDTO>> get() {
+//        List<Member> member = memberRepository.findAll();
+//        List<MemberDTO> memberDTOList = member.stream()
+//                .map(m -> anyMapTo(m, MemberDTO.class))
+//                .collect(Collectors.toList());
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(memberDTOList);
+//    }
+//
+//    @PutMapping("/{id}")
+//    @ResponseBody
+//    ResponseEntity<MemberDTO> put(@PathVariable Long id, @RequestBody final MemberDTO memberDTO) {
+//
+//        Optional<Member> memberOp = memberRepository.findById(id);
+//        Member member = memberOp.orElseThrow();
+//
+//        modelMapper.map(memberDTO, member);
+//
+//        memberRepository.save(member);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(memberDTO);
+//    }
+//
+//    private Member mapTo(final MemberDTO memberDTO) {
+//        return modelMapper.map(memberDTO, Member.class);
+//    }
+//
+//    private <T> T anyMapTo(Object data, Class<T> type) {
+//        return modelMapper.map(data, type);
+//    }
     //test-001
     //test-002
     //test-003
